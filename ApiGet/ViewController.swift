1d460510d2dc6be824b7a19bcae923b232769c3c
Sabbir Hossain <smac@labs-mac.local>
2020-01-13 17:23:34 +0600

    API url session data show

--- ApiGet/ViewController.swift
+++ ApiGet/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtview: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btn(_ sender: Any) {
+        
+        guard let url = URL(string: "https://api.darksky.net/forecast/5c74c06e261c64b3a2f6e1abfb7fe85d/37.8267,-122.4233") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                print(data)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                    self.txtview.text = String(bytes: data, encoding: .utf8)
+                } catch {
+                    print(error)
+                }
+                
+            }
+            }.resume()
 
+        
+    }
+    
+    
+    
 }
 
